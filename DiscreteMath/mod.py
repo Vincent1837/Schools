3e0816f388ac937aa46f7755b8acb1fc1935aabd
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2021-11-05 04:16:11 +0800

    Discrete Math HW2 done
    Done and uploaded

--- DiscreteMath/mod.py
+++ DiscreteMath/mod.py
@@ -1,16 +1,2 @@
-x = 1
-m = 101
-power = (123**2) % m
-bin_str = bin(1001)[:1:-1]
-print(bin_str)
-for i in bin_str:
-    print("char : ", i)
-    if i == "1":
-        x = (x * power) % m
-        
-    else:
-        power = (power*power) % m
-    print("x, power : ", x, power)
-        
-
-print(x)
+str = "they are only good at rehearsing. One becomes an expert as long as others consider him/her to be one, after all, who will call himself/herself an expert?".split()
+print(len(str))
