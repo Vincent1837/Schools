d6a2f6e2bf8736ded1fe919496a37bb657577c3b
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2021-11-12 00:16:11 +0800

    CS Assignment7-1 Done

---
+++ CS/A7-110502567/A7-110502567-1.py
@@ -0,0 +1,26 @@
+"""
+Assignment 7
+Name: 蔡淵丞
+Student Number: 110502567
+Course: 2021-CE1003-B
+"""
+with open("seat.txt", "r") as data:
+    seat = [int(x) for x in data.read()]
+
+seat_taken = [x for x in range(len(seat)) if seat[x] == 1]
+
+count = 1
+while {0}.issubset(set(seat)):
+    for i in seat_taken:
+        if i-count >= 0:
+            if seat[i-count] == 0:
+                seat[i-count] = count+1
+        if i+count < len(seat):
+            if seat[i+count] == 0:
+                seat[i+count] = count+1
+    count += 1
+
+farest_seats = [x for x in range(len(seat)) if seat[x] == count]
+L = count-1
+print("L = {}".format(L))
+print("i = {}".format(farest_seats))
