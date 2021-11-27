829c05ad9f03166b65dcf2ddff098c229d1d57c2
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2021-11-28 04:10:03 +0800

    testing

--- CS/A7-110502567/t.py
+++ CS/A7-110502567/t.py
@@ -1,3 +1,5 @@
-a = 1
-b = 2
-a, b = 3
+def print_parentheses(n: int):
+    if n == 1:
+        return {"()"}
+    return {"("+i+")" for i in print_parentheses(n-1)} | {"()"+i for i in print_parentheses(n-1)} | {i+"()" for i in print_parentheses(n-1)}
+print(" \n".join(print_parentheses(int(input()))))
