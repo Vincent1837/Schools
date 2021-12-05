f2f81f9fe3d23d32cfb0ef20f153bda767465c5b
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2021-12-05 19:13:52 +0800

    .

--- DiscreteMath/mod.py
+++ DiscreteMath/mod.py
@@ -1,2 +1,12 @@
-lst = [x for x in range(1000, 10000) if x%35 == 0]
-print(12*11*10*9*8*7/(6*5*4*3*2))
+def factorial(n):
+    if n == 1 or n == 0:
+        return 1
+    return n * factorial(n-1)
+
+def p(n:int, r:int):
+    return factorial(n)//factorial(n-r)
+
+def c(n:int, r:int):
+    return factorial(n)//(factorial(r)*factorial(n-r))
+
+print(c(12,5)-c(9,5)-c(7,5))
