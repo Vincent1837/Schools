f108721d5c40d3be87606ba12b60e268968bd15d
Vincent1837 <86534155+Vincent1837@users.noreply.github.com>
2022-01-13 22:19:23 +0800

    .

--- DiscreteMath/mod.py
+++ DiscreteMath/mod.py
@@ -1,1 +1,19 @@
-input ()
+import random
+a = random.randrange(1, 100)
+b = random.randrange(1, 100)
+ans = a - b
+
+while 1:
+    print(a, '-', b)
+    get = int(input("答案是..."))
+    if get == ans:
+        print("答對了，好棒！")
+        break
+    else:
+        get = int(input("答錯了，請重算一次，答案是..."))
+        if get == ans:
+            print("答對了！")
+            break
+        else:
+            print("答錯了！正確答案是", ans)
+            break
