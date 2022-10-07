335eb6a04c270000ff85a172f01c894b41ec13e2
蔡淵丞 <86534155+Vincent1837@users.noreply.github.com>
2022-10-07 18:17:13 +0800

    stack.c not done

--- Data Structures/stack.c
+++ Data Structures/stack.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct stack
-{
+struct stack {
     int maxSize;
     int top;
     char *items;
 };
 
-struct stack* newStack (int capacity)
-{
+struct stack* newStack (int capacity) {
     struct stack *pt = (struct stack*)malloc(sizeof(struct stack));
     pt->maxSize = capacity;
     pt->top = -1;
@@ -19,70 +17,95 @@ struct stack* newStack (int capacity)
     return pt;
 }
 
-int size (struct stack *pt)
-{
+int size (struct stack *pt) {
     return pt->top + 1;
 }
 
-int isFull (struct stack *pt)
-{
+int isFull (struct stack *pt) {
     return pt->top == pt->maxSize - 1;  
 }
 
-int isEmpty (struct stack *pt)
-{
+int isEmpty (struct stack *pt) {
     return pt->top == -1;
 }
 
-void push (struct stack *pt, char c)
-{
-    if (isFull(pt))
-    {
+void push (struct stack *pt, char c) {
+    if (isFull(pt)) {
         printf("Overflow\nProgram Terminated\n");
         exit(EXIT_FAILURE);
     }
-    // printf("Inserting %.2f\n", f);
     pt->items[++pt->top] = c;
 }
 
-char peek (struct stack *pt)
-{
-    if (!isEmpty(pt))
-    {
+char peek (struct stack *pt) {
+    if (!isEmpty(pt)) {
         return pt->items[pt->top];
-    } else
-    {
+    } else {
         exit(EXIT_FAILURE);
     }
 }
 
-char pop (struct stack *pt)
-{
-    if (isEmpty(pt))
-    {
+char pop (struct stack *pt) {
+    if (isEmpty(pt)) {
         printf("Underflow\nProgram Terminated\n");
         exit(EXIT_FAILURE);
     }
-    // printf("Removing %.2f\n", peek(pt));
     return pt->items[pt->top--];
 }
 
-int main(){
-    struct stack *pt = newStack(50);
+int getPriority (char c) {
+    if (c == '+' || c == '-') {
+        return 1;
+    } else if (c == '*' || c == '/') {
+        return 2;
+    }
+    return 0;
+}
+
+int main() {
+
+    struct stack *operatorStack = newStack(50);
+    push(operatorStack, '\0');
     char infixStr[50];
     scanf("%s", infixStr);
     int length = (int)strlen(infixStr);
+    struct stack *prefixStack = newStack(50);
+    char prefixStr[length];
+    push(prefixStack, '\0');
 
     // Reverse the string
     char reversedStr[length];
-    char prefixStr[length];
     for (int i = 0; i < length; i++) {
         reversedStr[i] = infixStr[length - i - 1];
     }
     for (int i = 0; i < length; i++) {
         infixStr[i] = reversedStr[i];
     }
+    
+    // infix to postfix
+    for (int i = 0; i < length; i++) {
+        if (infixStr[i] >= '0' && infixStr[i] <= '9') {
+            push(prefixStack, infixStr[i]);
+        } else {
+            while (getPriority(infixStr[i]) < getPriority(peek(operatorStack))) {
+                push(prefixStack, pop(operatorStack));
+            }
+            push(operatorStack, infixStr[i]);
+        }
+    }
 
+    while (!isEmpty(operatorStack)) {
+        printf("%c", peek(operatorStack));
+        push(prefixStack, pop(operatorStack));
+    }
+
+    int loopCount = 0;
+    while (!isEmpty(prefixStack)) {
+        prefixStr[loopCount++] = pop(prefixStack);
+        printf("%d\n", loopCount);
+    }
+
+    printf("%s\n", prefixStr);
 
     return 0;
 }
