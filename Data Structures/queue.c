47559d81a9ad2ed1fb4834b5803a4d478eb801f8
蔡淵丞 <vincent@Chan.localdomain>
2022-10-20 02:23:11 +0800

    testing while and scanf

--- Data Structures/queue.c
+++ Data Structures/queue.c
@@ -4,59 +4,59 @@
 int main(){
     
     struct Queue {
-	int front, rear, size;
-	unsigned capacity;
-	int* array;
+		int front, rear, size;
+		unsigned capacity;
+		int* array;
     };
 
     struct Queue* creatQueue (unsigned capacity) {
-	struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
-	queue->capacity = capacity;
-	queue->front = queue->size = 0;
+		struct Queue* queue = (struct Queue*)malloc(sizeof(struct Queue));
+		queue->capacity = capacity;
+		queue->front = queue->size = 0;
 
-	queue->rear = queue->size - 1;
-	queue->array = (int*)malloc(queue->capacity * sizeof(int));
-
-	return queue;
+		queue->rear = queue->size - 1;
+		queue->array = (int*)malloc(queue->capacity * sizeof(int));
+	
+		return queue;
     }
 
     int isEmpty (struct Queue* queue) {
-	return queue->size == 0;
+		return queue->size == 0;
     }
     
     int isFull (struct Queue* queue) {
-	return queue->size == queue->capacity;
+		return queue->size == queue->capacity;
     }
 
     int size (struct Queue* queue) {
-	return queue->size;
+		return queue->size;
     }
 
     int front (struct Queue* queue) {
-	return queue->array[queue->front];
+		return queue->array[queue->front];
     }
 
     int rear (struct Queue* queue) {
-	return queue->array[queue->rear];
+		return queue->array[queue->rear];
     }
 
     void enqueue (struct Queue* queue, int n) {
-	if (isFull(queue)) {
-	    printf("Overflow!!!! Program terminated.\n");
-	    exit(EXIT_FAILURE);
-	} else {
-	    queue->array[++queue->rear] = n;
-	}
+		if (isFull(queue)) {
+	    	printf("Overflow!!!! Program terminated.\n");
+	   		exit(EXIT_FAILURE);
+		} else {
+	    	queue->array[++queue->rear] = n;
+		}
     }
 
     int dequeue (struct Queue* queue) {
-	if (isEmpty(queue)) {
-	    printf("Underflow!!!! Program terminated.\n");
-	    exit(EXIT_FAILURE);
-	} else {
-	    return queue->array[queue->front++];
-	}
+		if (isEmpty(queue)) {
+	    	printf("Underflow!!!! Program terminated.\n");
+	    	exit(EXIT_FAILURE);
+		} else {
+		    return queue->array[queue->front++];
+		}
     }
-
+	
     return 0;
 }
