4e7a8efbbda1a5a7bc2d5eb5d1c7cef21058a189
蔡淵丞 <86534155+Vincent1837@users.noreply.github.com>
2022-12-22 05:16:37 +0800

    Update hw4_1.c

--- Data_Structures/hw4/hw4_1.c
+++ Data_Structures/hw4/hw4_1.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
 struct edge {
-    int u, v w;
-    
-} edge;
+    int u, v, w;
+};
+
+struct graph {
+    int V, E;
+    struct edge* edges;
+};
+
+struct graph* make_graph () {
+    struct graph* new_graph;
+    new_graph->E = 0;
+    new_graph->V = 0;
+    new_graph->edges = (struct edge *)malloc(sizeof(struct edge));
+}
+
+
+
+void insert_edge (struct graph* graph, u,v ) {
+
+}
 
 
 int main () {
