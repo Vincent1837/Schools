c967e80040271fd82934e4a3832a765794c55f04
蔡淵丞 <86534155+Vincent1837@users.noreply.github.com>
2023-03-13 03:03:27 +0800

    PL

--- Calculus/hw/differentiation.py
+++ Calculus/hw/differentiation.py
@@ -10,21 +10,21 @@ def factorial(n):
 def taylorPloy(n, m):
     s = 0
     for k in range(n):
-        s += ((-1)**(k) * m**(k + 1)) / factorial(k)
+        s += (-1)**pylab.floor(k/2) * (m**k / factorial(k))
     return s
 
 x = numpy.linspace(0, 10.5, 100)
 n = 20
 
-for i in range(1, n+1):
+for i in range(1, n, 2):
     y = taylorPloy(i, x)
     pylab.plot(x, y, label='p'+str(i))
-pylab.plot(x, x*pylab.exp(-x), label="xexp(-x)")
+pylab.plot(x, pylab.sin(x)+pylab.cos(x), label="sin(x)+cos(x)")
 
-pylab.title("Taylor ploynomials with different orders for x exp(-x)")
+pylab.title("Taylor ploynomials with different orders for sin(x)+cos(x)")
 pylab.xlabel("x")
 pylab.ylabel("y")
 pylab.grid()
 pylab.legend()
-pylab.ylim(-2, 3)
+pylab.ylim(-2, 2)
 pylab.show()
